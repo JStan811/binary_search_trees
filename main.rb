afc974ae8047bdac249e0502263f02a04b3247b6
Jake Standley <jakestandley95@gmail.com>
2021-09-15 16:37:32 -0500

    Added methods to tree class for insert, delete, find, level_order, inorder, preorder, postorder, and height

--- main.rb
+++ main.rb
@@ -3,14 +3,29 @@
 require_relative 'node'
 require_relative 'tree'
 
-array_of_random_numbers = (Array.new(15) { rand(1..100) })
+# array_of_random_numbers = (Array.new(15) { rand(1..100) })
 
-p array_of_random_numbers
+array_of_random_numbers = [48, 84, 5, 95, 30, 66, 8, 26, 75, 17, 23, 79, 22, 80, 56]
+# array_simple = [1, 2, 3, 4, 5, 6, 7]
 
 my_tree = BinarySearchTrees::Tree.new(array_of_random_numbers)
+# my_tree_simple = BinarySearchTrees::Tree.new(array_simple)
+# my_tree_empty = BinarySearchTrees::Tree.new
 
-puts my_tree
+# my_tree_simple.pretty_print
 
-p my_tree
+# puts my_tree_simple.height(my_tree_simple.find(7))
+
+# my_tree_simple.insert(3.5)
+
+# my_tree_simple.pretty_print
+
+# puts my_tree_simple.height_using_recursion(my_tree_simple.find(4))
+
+my_tree.insert(45)
+
+my_tree.insert(44)
 
 my_tree.pretty_print
+
+puts my_tree.height(my_tree.find(48))
--- tree.rb
+++ tree.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module BinarySearchTrees
+  # rubocop: disable Metrics/ClassLength
   # object representing a Binary Search Tree
   class Tree
+    attr_reader :root
+
     def initialize(array = [])
       # sort and remove duplicates
       @array = array.sort.uniq
@@ -15,10 +18,191 @@ module BinarySearchTrees
       pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
     end
 
+    # rubocop: disable Metrics/MethodLength
+    # rubocop: disable Metrics/AbcSize
+    # rubocop: disable Metrics/PerceivedComplexity
     def insert(value)
+      unless find(value).nil?
+        puts 'Value already exists in tree.'
+        return
+      end
+
+      new_node = Node.new(value)
+      current_node = @root
+      until current_node.left_child.nil? && current_node.right_child.nil?
+        current_node = if value < current_node.data && !current_node.left_child.nil?
+                         current_node.left_child
+                       else
+                         current_node.right_child
+                       end
+      end
+
+      if value < current_node.data
+        current_node.left_child = new_node
+      else
+        current_node.right_child = new_node
+      end
+      new_node
+    end
+    # rubocop: enable Metrics/MethodLength
+    # rubocop: enable Metrics/AbcSize
+    # rubocop: enable Metrics/PerceivedComplexity
+
+    # rubocop: disable Metrics/MethodLength
+    # rubocop: disable Metrics/AbcSize
+    # rubocop: disable Metrics/PerceivedComplexity
+    # rubocop: disable Metrics/CyclomaticComplexity
+    def delete(value)
+      if find(value).nil?
+        puts 'Value does not exist in tree.'
+        return
+      end
+
+      current_node = @root
+      previous_node = nil
+      until current_node.data == value
+        previous_node = current_node
+        current_node = if value < current_node.data && !current_node.left_child.nil?
+                         current_node.left_child
+                       else
+                         current_node.right_child
+                       end
+      end
+
+      # case where node to be deleted is a leaf
+      if current_node.left_child.nil? && current_node.right_child.nil?
+        if previous_node.left_child == current_node
+          previous_node.left_child = nil
+        else
+          previous_node.right_child = nil
+        end
+      # case where node to be deleted has only one child
+      elsif current_node.left_child.nil?
+        if previous_node.left_child == current_node
+          previous_node.left_child = current_node.right_child
+        else
+          previous_node.right_child = current_node.right_child
+        end
+      elsif current_node.right_child.nil?
+        if previous_node.left_child == current_node
+          previous_node.left_child = current_node.left_child
+        else
+          previous_node.right_child = current_node.left_child
+        end
+      # case where node to be deleted has two children
+      else
+        next_highest_node = current_node.right_child
+        next_highest_node_previous_node = current_node
+        until next_highest_node.left_child.nil?
+          next_highest_node_previous_node = next_highest_node
+          next_highest_node = next_highest_node.left_child
+        end
+        # covers case where the next highest node has a right child
+        if !(next_highest_node.right_child.nil?)
+          next_highest_node_previous_node.left_child = next_highest_node.right_child
+        else
+          next_highest_node_previous_node.left_child = nil
+        end
+        next_highest_node.left_child = current_node.left_child
+        next_highest_node.right_child = current_node.right_child unless current_node.right_child == next_highest_nodel
+        if current_node == @root
+          @root = next_highest_node
+        elsif previous_node.left_child == current_node
+          previous_node.left_child = next_highest_node
+        else
+          previous_node.right_child = next_highest_node
+        end
+      end
+      current_node
+    end
+    # rubocop: enable Metrics/MethodLength
+    # rubocop: enable Metrics/AbcSize
+    # rubocop: enable Metrics/PerceivedComplexity
+    # rubocop: enable Metrics/CyclomaticComplexity
+
+    def find(value, root = @root)
+      return root if root.nil? || root.data == value
+
+      if value < root.data
+        find(value, root.left_child)
+      else
+        find(value, root.right_child)
+      end
     end
 
-    def delete(value)
+    # rubocop: disable Metrics/MethodLength
+    # rubocop: disable Metrics/AbcSize
+    def level_order_using_iteration
+      # guard clause in case tree is empty
+      return if @root.nil?
+
+      result_array = []
+      queue = []
+
+      current_node = @root
+      result_array << current_node.data
+      queue << current_node.left_child unless current_node.left_child.nil?
+      queue << current_node.right_child unless current_node.right_child.nil?
+
+      until queue.empty?
+        current_node = queue.shift
+        result_array << current_node.data
+        queue << current_node.left_child unless current_node.left_child.nil?
+        queue << current_node.right_child unless current_node.right_child.nil?
+      end
+
+      result_array
+    end
+    # rubocop: enable Metrics/MethodLength
+    # rubocop: enable Metrics/AbcSize
+
+    def level_order_using_recursion(root = @root, result_array = [], queue = [])
+      # base case
+      return result_array if root.nil?
+
+      result_array << root.data
+      queue << root.left_child unless root.left_child.nil?
+      queue << root.right_child unless root.right_child.nil?
+      root = queue.shift
+      level_order_using_recursion(root, result_array, queue)
+    end
+
+    def preorder(root = @root, result_array = [])
+      # base case
+      return result_array if root.nil?
+
+      result_array << root.data
+      preorder(root.left_child, result_array)
+      preorder(root.right_child, result_array)
+    end
+
+    def inorder(root = @root, result_array = [])
+      # base case
+      return result_array if root.nil?
+
+      inorder(root.left_child, result_array)
+      result_array << root.data
+      inorder(root.right_child, result_array)
+    end
+
+    def postorder(root = @root, result_array = [])
+      # base case
+      return result_array if root.nil?
+
+      postorder(root.left_child, result_array)
+      postorder(root.right_child, result_array)
+      result_array << root.data
+    end
+
+    def height(node, level_change_count = 0, height = 0)
+      # base case
+      return height if node.data == inorder.last
+
+      height = level_change_count if height < level_change_count
+
+      height = height(node.left_child, level_change_count + 1, height) unless node.left_child.nil?
+      height = height(node.right_child, level_change_count + 1, height) unless node.right_child.nil?
+      height
     end
 
     private
@@ -36,4 +220,5 @@ module BinarySearchTrees
       root
     end
   end
+  # rubocop: enable Metrics/ClassLength
 end
